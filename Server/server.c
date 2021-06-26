e2fa9a95cb3475a63b45a066a43934c860fe3f1b
Cristeee <tomoiucris787@gmail.com>
2021-06-26 16:37:22 +0300

    Admin component and few corrections

--- Server/server.c
+++ Server/server.c
@@ -1,4 +1,3 @@
-
 #include<stdio.h>
 #include<string.h>	//strlen
 #include<stdlib.h>	//strlen
@@ -7,30 +6,38 @@
 #include<unistd.h>	//write
 #include<pthread.h> //for threading , link with lpthread
 
-//the thread function
+int adminIsConnected;
+
 void *connection_handler(void *);
+void *wait_for_web_connections(void *);
+void *web_connection_handler(void *);
+void *wait_for_admin_connection(void *);
+void *admin_connection_handler(void *);
 
 int main(int argc , char *argv[]) {
+
+    adminIsConnected = 0;
+
+    pthread_t webConnectionsHandler;
+    pthread_create(&webConnectionsHandler, NULL, wait_for_web_connections, NULL);
+
+    pthread_t adminConnectionsHandler;
+    pthread_create(&adminConnectionsHandler, NULL, wait_for_admin_connection, NULL);
+
     int socket_desc, client_sock, c, *new_sock;
     struct sockaddr_in server, client, webconnection;
-
-
 
     //Create socket
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_desc == -1) {
         printf("Could not create socket");
     }
-    puts("Socket created");
+    puts("[+] Socket created");
 
     //Prepare the sockaddr_in structure
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(8888);
-    webconnection.sin_family = AF_INET;
-    webconnection.sin_addr.s_addr = INADDR_ANY;
-    webconnection.sin_port = htons(8080);
-
 
     //Bind
     if (bind(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0) {
@@ -38,14 +45,7 @@ int main(int argc , char *argv[]) {
         perror("bind failed. Error");
         return 1;
     }
-    puts("bind done");
-    //Bind
-    if (bind(socket_desc, (struct sockaddr *) &webconnection, sizeof(webconnection)) < 0) {
-        //print the error message
-        perror("bind failed. Error pentru web ");
-        return 1;
-    }
-    puts("bind done for web");
+    puts("[+] Bind done");
 
     //Listen
     listen(socket_desc, 3);
@@ -53,94 +53,174 @@ int main(int argc , char *argv[]) {
     //Accept and incoming connection
     puts("Waiting for incoming connections...");
     c = sizeof(struct sockaddr_in);
-
-
-    //Accept and incoming connection
-    puts("Waiting for incoming connections...");
-    c = sizeof(struct sockaddr_in);
     while ((client_sock = accept(socket_desc, (struct sockaddr *) &client, (socklen_t *) &c))) {
-        puts("Connection accepted");
+        puts("[+] Connection accepted");
 
         pthread_t sniffer_thread;
         new_sock = malloc(1);
         *new_sock = client_sock;
 
         if (pthread_create(&sniffer_thread, NULL, connection_handler, (void *) new_sock) < 0) {
-            perror("could not create thread");
+            perror("Could not create thread");
             return 1;
-        }
-
-        //Now join the thread , so that we dont terminate before the thread
-        //pthread_join( sniffer_thread , NULL);
-        puts("Handler assigned");
-        char client_message[200]= {0};
-        char message[100] = {0};
-        const char *pMessage = "hello aticleworld.com";
-        memset(client_message, '\0', sizeof client_message);
-        memset(message, '\0', sizeof message);
-        //Receive a reply from the client
-        while (1){
-        if( recv(client_sock, client_message, 200, 0) < 0)
-        {
-            printf("recv failed");
-            break;
-        }
-        printf("Client reply : %s\n",client_message);
-        if(strcmp(pMessage,client_message)==0)
-        {
-            strcpy(message,"Hi there !");
-        }
-        else
-        {
-            strcpy(message,"Invalid Message !");
-        }
         }
     }
 
     if (client_sock < 0) {
-        perror("accept failed");
+        perror("Accept failed");
         return 1;
     }
-
 
     return 0;
 }
 
+void *wait_for_admin_connection(void *argv)
+{
+    struct sockaddr_in server, client;
+    int client_sock, c;
+    int* new_sock;
+    //Create socket
+    int socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_desc == -1) {
+        printf("Could not admin create socket");
+    }
+    puts("[+] Admin socket created");
 
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(9090);
 
+    //Bind
+    if (bind(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0) {
+        //print the error message
+        perror("Admin bind failed. Error");
+        return 1;
+    }
+    puts("[+] Admin bind done");
 
-/*
- * This will handle connection for each client
- * */
+    //Listen
+    listen(socket_desc, 3);
+
+    //Accept and incoming connection
+    puts("Waiting for incoming admin connections...");
+    c = sizeof(struct sockaddr_in);
+    if ((client_sock = accept(socket_desc, (struct sockaddr *) &client, (socklen_t *) &c)) && adminIsConnected > 0) {
+        adminIsConnected = 1;
+        puts("[+] Admin connection accepted");
+
+        pthread_t sniffer_thread;
+        new_sock = malloc(1);
+        *new_sock = client_sock;
+
+        if (pthread_create(&sniffer_thread, NULL, admin_connection_handler, (void *) new_sock) < 0) {
+            perror("Could not create admin thread");
+            return 1;
+        }
+    }
+
+    if (client_sock < 0) {
+        perror("Admin accept failed");
+        return 1;
+    }
+}
+
+void *wait_for_web_connections(void *argv)
+{
+    struct sockaddr_in server, client;
+    int client_sock, c;
+    int* new_sock;
+    //Create socket
+    int socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_desc == -1) {
+        printf("Could not web create socket");
+    }
+    puts("[+] Web socket created");
+
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(8080);
+
+    //Bind
+    if (bind(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0) {
+        //print the error message
+        perror("web bind failed. Error");
+        return 1;
+    }
+    puts("[+] Web bind done");
+
+    //Listen
+    listen(socket_desc, 3);
+
+    //Accept and incoming connection
+    puts("Waiting for incoming WEB connections...");
+    c = sizeof(struct sockaddr_in);
+    while ((client_sock = accept(socket_desc, (struct sockaddr *) &client, (socklen_t *) &c))) {
+        puts("[+] Web connection accepted");
+
+        pthread_t sniffer_thread;
+        new_sock = malloc(1);
+        *new_sock = client_sock;
+
+        if (pthread_create(&sniffer_thread, NULL, web_connection_handler, (void *) new_sock) < 0) {
+            perror("Could not create web thread");
+            return 1;
+        }
+    }
+
+    if (client_sock < 0) {
+        perror("Web accept failed");
+        return 1;
+    }
+}
+
+void *admin_connection_handler(void *argv)
+{
+    int sock = *(int*)argv;
+    char adminMessage[2048];
+
+    recv(sock , adminMessage, sizeof(adminMessage), 0);
+    printf("-------------\nFrom admin\n-------------\n%s\n\n", adminMessage);
+
+    bzero(&adminMessage, sizeof(adminMessage));
+}
+
+void *web_connection_handler(void *argv)
+{
+    int sock = *(int*)argv;
+    char webMessage[2048];
+
+    recv(sock , webMessage, sizeof(webMessage), 0);
+    printf("-------------\nFrom web page\n-------------\n%s\n\n", webMessage);
+
+    bzero(&webMessage, sizeof(webMessage));
+}
+
 void *connection_handler(void *socket_desc)
 {
     //Get the socket descriptor
     int sock = *(int*)socket_desc;
     int read_size;
-    char *message , client_message[2000];
-
-    //Send some messages to the client
-    message = "Greetings! I am your connection handler\n";
-    write(sock , message , strlen(message));
-
-    message = "Now type something and i shall repeat what you type \n";
-    write(sock , message , strlen(message));
+    char client_message[2000];
 
     //Receive a message from client
     while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
     {
-        //Send the message back to client
-        write(sock , client_message , strlen(client_message));
-    }
+        fprintf(stdout, "--------------\nClient NO: %d\n--------------\n%s\n\n", sock, client_message);
+        if(read_size <= 0)
+        {
+            puts("Client disconnected");
+            fflush(stdout);
+        } else if(read_size == -1)
+        {
+            perror("recv failed");
+        }
 
-    if(read_size == 0)
-    {
-        puts("Client disconnected");
-        fflush(stdout);
-    }
-    else if(read_size == -1)
-    {
-        perror("recv failed");
+        char messageToCLient[2048] = {"Sample message. Client does nothing"};
+
+        send(sock , messageToCLient , strlen(messageToCLient) , 0);
+        bzero(&client_message, sizeof(client_message));
     }
 
     //Free the socket pointer
--- client/client.c
+++ client/client.c
@@ -1,34 +1,26 @@
-
 #include <stdio.h>	//printf
+#include <stdlib.h>
 #include <string.h>	//strlen
 #include <sys/socket.h>	//socket
 #include <arpa/inet.h>	//inet_addr
 #include <unistd.h>
+#include <pthread.h>
 
-//trimitem data catre server cu timeout de 20 de sec
-int SockSend(int sock, char* Rqst, short lenRqst)
+struct s_message
 {
-    int shortRetval = -1;
-    struct timeval tv;
-    tv.tv_sec = 20;
-    tv.tv_usec = 0;
+    int vitezaVant;
+    int vitezaRotEoliana;
+    int curentProdus;
+};
 
-    if(setsockopt(sock,SOL_SOCKET, SO_SNDTIMEO,(char *)&tv, sizeof (tv)) < 0)
-    {
-        printf("Time out\n");
-        return -1;
-    }
-    shortRetval = send(sock, Rqst, lenRqst,0);
-    return shortRetval;
-
-}
+void createMessage(char* message, struct s_message s_message_);
+void *handler_recv_thread(void *argv);
 
 int main(int argc , char *argv[])
 {
     int sock;
     struct sockaddr_in server;
     char message[1000] , server_reply[2000];
-
 
     //Create socket
     sock = socket(AF_INET , SOCK_STREAM , 0);
@@ -38,6 +30,15 @@ int main(int argc , char *argv[])
     }
     puts("Socket created");
 
+    struct timeval timeout;      
+    timeout.tv_sec = 10;
+    timeout.tv_usec = 0;
+    
+    if (setsockopt (sock, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0)
+    {
+        error("setsockopt failed\n");
+    }
+        
     server.sin_addr.s_addr = inet_addr("127.0.0.1");
     server.sin_family = AF_INET;
     server.sin_port = htons( 8888 );
@@ -45,47 +46,85 @@ int main(int argc , char *argv[])
     //Connect to remote server
     if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
     {
-        perror("connect failed. Error");
+        perror("Connect failed. Error");
         return 1;
     }
 
-    puts("Connected\n");
-
-
+    puts("Connected");
 
     int vitezaVant=21;
     int vitezaEoliana=15;
     int curentProdus=10;
     int SendToServer[100] = {vitezaVant,vitezaEoliana,curentProdus};
 
+    int *new_sock = &sock;
+    pthread_t waitForMessageThread;
+    pthread_create(&waitForMessageThread, NULL, handler_recv_thread, (void *) new_sock);
+
     //keep communicating with server
     while(1)
     {
-        printf("Datele trimise sunt: ",vitezaEoliana,vitezaVant,curentProdus  );
-        gets(SendToServer);
-        SockSend(sock, SendToServer, strlen(SendToServer));
+        char message[2000];
 
-        //Send some data
+        struct s_message dummy_message;
+        dummy_message.vitezaVant = 21;
+        dummy_message.vitezaRotEoliana = 15;
+        dummy_message.curentProdus = 10;
+
+        createMessage(message, dummy_message);
+
         if( send(sock , message , strlen(message) , 0) < 0)
         {
             puts("Send failed");
             return 1;
         }
-
-        //Receive a reply from the server
-        if( recv(sock , server_reply , 2000 , 0) < 0)
-        {
-            puts("recv failed");
-            break;
-        }
-
-        puts("Server reply :");
-        puts(server_reply);
-
-
-
+        bzero(&message, sizeof(message));
+        sleep(1);
     }
 
     close(sock);
     return 0;
+}
+
+void *handler_recv_thread(void *argv)
+{
+    int sock = *(int*)argv;
+
+    char message[2048];
+
+    while (1)
+    {
+        if (recv(sock , message , sizeof(message), 0) >= 0)
+        {
+            fprintf(stdout, "Server said: %s\n", message);
+
+            // check if message == something
+            bzero(&message, sizeof(message));
+        };
+    }
+    
+
+}
+
+void createMessage(char* message, struct s_message s_message_)
+{
+    char bufferWindSpeed[256];
+    char bufferTurbineRot[256];
+    char bufferEnergyProduced[256];
+
+    snprintf(bufferWindSpeed, sizeof(bufferWindSpeed), "%d", s_message_.vitezaVant);
+    snprintf(bufferTurbineRot, sizeof(bufferTurbineRot), "%d", s_message_.vitezaRotEoliana);
+    snprintf(bufferEnergyProduced, sizeof(bufferEnergyProduced), "%d", s_message_.curentProdus);
+
+    strcat(message, "Viteza vant:            ");
+    strcat(message, bufferWindSpeed);
+    strcat(message, "kts\n");
+
+    strcat(message, "Viteza rotatie eoliana: ");
+    strcat(message, bufferTurbineRot);
+    strcat(message, "rpm\n");
+
+    strcat(message, "Curent produs:          ");
+    strcat(message, bufferEnergyProduced);
+    strcat(message, "W");
 }
